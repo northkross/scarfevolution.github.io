120137a7a029ce49a74aaa98ba8662b1f8c2df62
northkross <dallasmatt14@gmail.com>
2025-02-03 11:33:24 -0500

    Update junction.js

--- junction.js
+++ junction.js
@@ -1,7 +1,7 @@
 
 const combox = document.getElementById("comment");
 const combutt = document.getElementById("submit");
-
+const commentSection = document.getElementById("box");
  combutt.addEventListener("click", function () {
  const commentText = combox.value.trim();
 
@@ -9,6 +9,7 @@ const moreComm = document.createElement("div");
    moreComm.classList.add("comment");
    moreComm.textContent = commentText;
 
+  commentSection.appendChild(moreComm);
 
    combox.value = "";
  }
