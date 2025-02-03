fafa41d182793a6a754e772075a59ed2e769e71c
northkross <dallasmatt14@gmail.com>
2025-02-02 19:46:38 -0500

    Update junction.js

--- junction.js
+++ junction.js
@@ -1,1 +1,7 @@
+function commmfunc(){
+combox.write = combox;
+}
+const combox = document.getElementById("comment");
+const combutt = document.getElementById("submit");
 
+combutt.onclick = commfunc;
