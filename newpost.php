41606fcf1cb63c964a5d06a8edcc59682088f7a7
northkross <dallasmatt14@gmail.com>
2025-02-04 13:37:13 -0500

    Update newpost.php

--- newpost.php
+++ newpost.php
@@ -1,2 +1,17 @@
+<html>
+  <link type="text/css" rel="stylesheet" href="drip.css" />
+  
+  <head>
+    <title>7135E Website</title>
+  </head>
+
+  <body>
+    <h1>Under Construction</h1>
+    <img src="https://raw.githubusercontent.com/northkross/scarfevolution.github.io/refs/heads/main/7135EColor.png" alt="logo">
       <textarea id="comment" rows="30" cols="130"></textarea>
       <button id="submit">Submit</button>
+    
+  </body>
+  <script src="junction.js"></script>
+</html>
+
