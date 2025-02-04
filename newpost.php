da90f7cc9eacc3766985c929a770aef9d9eb8997
northkross <dallasmatt14@gmail.com>
2025-02-04 13:42:38 -0500

    Update newpost.php

--- newpost.php
+++ newpost.php
@@ -7,7 +7,6 @@
 
   <body>
     <h1>Under Construction</h1>
-    <img src="https://raw.githubusercontent.com/northkross/scarfevolution.github.io/refs/heads/main/7135EColor.png" alt="logo">
       <textarea id="comment" rows="30" cols="130"></textarea>
       <button id="submit">Submit</button>
     
