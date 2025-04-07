26c70b51a6e4dd2b989819b9537aadee197c175b
northkross <dallasmatt14@gmail.com>
2025-04-07 15:29:03 -0400

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -7,5 +7,8 @@ if (!isset($_SESSION["username"])) {
 ?>
 
 <h2>Welcome, <?php echo $_SESSION["username"]; ?>!</h2>
+<form action="delete_account.php" method="post" onsubmit="return confirm('Are you sure you want to delete your account? This cannot be undone!');">
+    <input type="submit" name="delete" value="Delete My Account">
+</form>
 <a href="logout.php">Logout</a>
 
